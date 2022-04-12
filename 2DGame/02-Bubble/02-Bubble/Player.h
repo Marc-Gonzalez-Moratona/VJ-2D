8a7c2c864e3109927a9db5d31bafdc39ae6252c2
Marc-Gonzalez-Moratona <95937384+Marc-Gonzalez-Moratona@users.noreply.github.com>
2022-04-12 03:15:27 +0200

    Update Player.h

--- 2DGame/02-Bubble/02-Bubble/Player.h
+++ 2DGame/02-Bubble/02-Bubble/Player.h
@@ -16,19 +16,23 @@ class Player
 public:
 	void init(const glm::ivec2& tileMapPos, ShaderProgram& shaderProgram);
 	void update(int deltaTime, int level);
-	void render();
+	void render(int level);
 
 	void setTileMap(TileMap* tileMap);
 	void setPosition(const glm::vec2& pos);
 	void setInitialPosition(int X, int Y);
+	bool spriteCollision(Sprite *s1, Sprite *s2, bool isWall);
+	void resetLevel();
+	void setStrawberryDispl(glm::fvec2 strawberryDispl);
 
 private:
-	bool bJumping, bClimbing, bClimbJumping, bDashing, bGrabbing;
+	bool bJumping, bClimbing, bClimbJumping, bDashing, bGrabbing, bWallBroken, bStrawberryCollected, strawberryUp;
 	glm::ivec2 tileMapDispl, posPlayer;
+	glm::fvec2 strawberryDispl;
 	int jumpAngle, climbJumpAngle, dashAngle, startY, initialX, initialY, dashDirection, dashY;
-	Texture spritesheet;
-	Sprite* sprite;
-	TileMap* map;
+	Texture spritesheet, strawberrysheet, springsheet, baloonsheet, wallsheet, wingsheet, platformsheet;
+	Sprite *sprite, *strawberry, *springLeft, *springRight, *baloon, *wall, *wing[2], *platforms[12];
+	TileMap *map;
 	bool bGodMode, bSlowMode, bDashMode, bChanging;
 
 };
